04eff440be03a5c0366e8e479c3ff0efe841bf62
mahdi ghaedi <codebymahdi@gmail.com>
2025-02-26 14:27:34 +0330

    fixes  3  course  Router mongoConnect 2

--- modules/courses/courses.routes.js
+++ modules/courses/courses.routes.js
@@ -29,9 +29,8 @@ const courseRouter = express.Router();
 // );
 
 // // روت برای دریافت دوره بر اساس شناسه یا دریافت همه دوره‌ها
+
 courseRouter.get("/:id?", getCourse);
-courseRouter.get("/site", site);
-courseRouter.get("/mite", mite);
 
 // // روت برای دریافت دوره بر اساس دست بندی
 // courseRouter.get("/cat/:category", getCourseByCat);
